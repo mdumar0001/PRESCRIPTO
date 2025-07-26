c14a404a4220a9c23743ecb0ff9984c698b1cf25
mdumar0001 <mdumar84002024@gmail.com>
2025-07-26 23:50:42 +0530

    homepage creat

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,32 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true); //token tue mean use logged in so we will not show the creat account button
   return (
     <div className=" flex justify-between items-center text-sm py-4 mb-5 border-b border-b-gray-400">
       <img className="w-44 cursor-pointer" src={assets.logo} />
-      <ul className="hidden  md:flex items-start gap-5 font-medium ">
-        <NavLink>
-          <li>HOME</li>
-          <hr />
-        </NavLink>{" "}
-        <NavLink>
-          <li>ALL DOCTORS</li>
-          <hr />
-        </NavLink>{" "}
-        <NavLink>
-          <li>ABOUT</li>
-          <hr />
-        </NavLink>{" "}
-        <NavLink>
-          <li>CONTACT</li>
-          <hr />
+      <ul className="hidden  md:flex items-start gap-5 font-medium">
+        <NavLink to="/">
+          <li className="py-1">HOME</li>
+          <hr className=" border-none outline-none h-0.5 bg-indigo-500 w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/doctors">
+          <li className="py-1">ALL DOCTORS</li>
+          <hr className=" border-none outline-none h-0.5 bg-indigo-500 w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/about">
+          {/* onclicking these navlink a active class will be added so we use it to style the actiavted link */}
+          <li className="py-1">ABOUT</li>
+          <hr className=" border-none outline-none h-0.5 bg-indigo-500 w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/contact">
+          <li className="py-1">CONTACT</li>
+          <hr className=" border-none outline-none h-0.5 bg-indigo-500 w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
-      <div>
-        <button className="border rounded bg-blue-500 p-2 text-white">
-          Create account
-        </button>
+      <div className="flex items-center gap-4">
+        {token ? (
+          <div className="flex items-center gap-2 cursor-pointer group relative">
+            <img className="w-8 rounded-full" src={assets.profile_pic} />
+            <img className="w-2.5" src={assets.dropdown_icon} alt="" />
+            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+              <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                <p
+                  onClick={() => navigate("/my-profile")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My Profile
+                </p>
+                <p
+                  onClick={() => navigate("/my-appointments")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My Appointments
+                </p>
+                <p
+                  onClick={() => setToken(false)}
+                  className="hover:text-black cursor-pointer"
+                >
+                  Logout
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <button
+            onClick={() => navigate("/login")}
+            className="border rounded-full bg-indigo-500 px-8 py-3 text-white font-light hidden md:block"
+          >
+            Create account
+          </button>
+        )}
       </div>
     </div>
   );
