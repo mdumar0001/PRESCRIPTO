eecffb315304ecdcaa6968a7d97c4c5dea9f7879
mdumar0001 <mdumar84002024@gmail.com>
2025-08-19 09:49:37 +0530

    fix: add base path for Render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,20 @@
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// export default defineConfig({
+//   plugins: [tailwindcss(), react()],
+//   server: { port: 5173 },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: { port: 5173 },
+  base: "/", // 👈 Render ke liye important
 });
